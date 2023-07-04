80f44d55b6b700908c1be88be6870e34cdb3801b
Bohdan-Parkhomenko <x231132007@gmail.com>
2023-07-04 14:12:57 +0300

    hi1

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import {Col, DropdownButton, Row, Dropdown, Button} from "react-bootstrap";
+import {Col, Row, Dropdown, Button} from "react-bootstrap";
 import './styles.css';
 import {stateItems} from "../../constants/stateItems";
 import Car from '../../assets/Truck - 1 1.png';
@@ -19,9 +19,8 @@ const Main = () => {
     }
 
 
-
     return (
-        <div className="vh-100 bg-gray-200">
+        <div className="vh-100 bg-gray-200 h-7">
             <p className="text-black text-xl font-normal leading-5 tracking-tight flex justify-center">
                 Start
             </p>
@@ -30,7 +29,8 @@ const Main = () => {
                 <Row className="flex justify-between">
                     {stateItems.map((item) => (
                         <Col className="flex flex-col items-center">
-                            <div className={`circle rounded-full flex items-center ${getCircleClass(item.index)}`}></div>
+                            <div
+                                className={`circle rounded-full flex items-center ${getCircleClass(item.index)}`}></div>
                             <p className="mt-2 text-black text-sm font-medium">{item.title}</p>
                         </Col>
                     ))}
@@ -38,13 +38,14 @@ const Main = () => {
             </div>
 
             <Row className="mb-3 ">
-                <Col  className="w-508 h-131 flex-shrink-0 rounded-lg bg-white shadow-md mr-3 ml-10">
+                <Col className="w-508 h-131 flex-shrink-0 rounded-lg bg-white shadow-md mr-3 ml-10">
 
                     <div className="pt-2 pr-2 pb-1 pl-2">
                         <p className="text-black text-lg font-medium leading-7 mb-3">Information</p>
 
                         <Row>
-                            <Col><p className="text-black text-default font-medium leading-6 mb-2">Info Number:</p></Col>
+                            <Col><p className="text-black text-default font-medium leading-6 mb-2">Info Number:</p>
+                            </Col>
                             <Col><p className={"text-right text-default text-gray-600"}>903049</p></Col>
                         </Row>
                         <Row>
@@ -75,7 +76,7 @@ const Main = () => {
             </Row>
 
             <div className="w-64.75rem h-18.4375rem flex-shrink-0 rounded-lg bg-white shadow-md mr-10 ml-10">
-                <div className="pt-2 pr-2 pb-1 pl-2">
+                <div className="pt-2 pr-2 pb-1 pl-2 ">
                     <p className={"text-container"}>Перезамовте Багаж</p>
 
                     <p className="mb-0 text-decoration-none flex flex-col justify-center flex-shrink-0 text-gray-600 text-sm font-normal leading-6">
@@ -130,7 +131,7 @@ const Main = () => {
                     </Dropdown>
 
 
-                    <div className="flex justify-end">
+                    <div className="flex justify-end ">
                         <Button className="text-white text-base px-4 py-2">
                             Done
                         </Button>
@@ -139,7 +140,7 @@ const Main = () => {
                 </div>
 
             </div>
-
+            <div className={"h-10"}/>
 
         </div>
 
